67441083b1c041d1dee050d53289ad12abb73684
Aldo Mercado <amercado1014@gmail>
2018-04-06 11:37:28 -0600

    Add restaurants with router

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './styles.css';
 import { Route } from 'react-router-dom';
 import { Main } from '../Main';
+import { Restaurants } from '../Restaurants'
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path='/' component={ Main } />
+        <Route path='/restaurants' component={ Restaurants } />
       </div>
     );
   }
---
+++ src/containers/Restaurants/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+export const Restaurants = () => {
+  return (
+    <div>
+      Restaurants!
+    </div>
+  );
+};
+
+export default Restaurants;
