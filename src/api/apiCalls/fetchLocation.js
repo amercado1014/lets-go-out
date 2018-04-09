f9f09fe2837e86dcf9447b4993d6a155720f038c
Aldo Mercado <amercado1014@gmail>
2018-04-09 11:23:13 -0600

    Add location fetch

---
+++ src/api/apiCalls/fetchLocation.js
@@ -0,0 +1,16 @@
+import { apiKey2 } from "./apiKey";
+
+export const fetchLocation = async () => {
+  const root = "https://www.googleapis.com/geolocation/v1";
+  const url = `${root}/geolocate?key=${apiKey2}`;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
+    });
+    const location = await response.json();
+    return location.location;
+  } catch (error) {
+    throw new Error("Error fetching location: ", error);
+  }
+};
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,8 +3,26 @@ import './styles.css';
 import { Route } from 'react-router-dom';
 import Header from '../Header';
 import RestaurantsContainer from '../RestaurantsContainer';
+import { fetchLocation } from '../../api/apiCalls/fetchLocation';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: ''
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const location = await fetchLocation();
+      console.log(location)
+    } catch (error) {
+      this.setState({error});
+    }
+  }
+
   render() {
     return (
       <div className="App">
