760663d020df774a9582575ddd989f55c11262f2
Aldo Mercado <amercado1014@gmail>
2018-04-09 19:55:44 -0600

    Add fetch restaurants by search to mocks

---
+++ src/api/apiCalls/__mocks__/fetchRestaurantsBySearch.js
@@ -0,0 +1,5 @@
+import { returnedMockRestaurantApiData } from "../../../mockData/mockData";
+
+export const fetchRestaurantsBySearch = jest.fn().mockImplementation(() => {
+  return Promise.resolve({ returnedMockRestaurantApiData });
+});
