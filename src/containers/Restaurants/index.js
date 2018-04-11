145d71721b8f9b1cbac3671b13ab8db90e2c105d
Aldo Mercado <amercado1014@gmail>
2018-04-10 18:08:43 -0600

    Add restaurants as a class

--- src/containers/Restaurants/index.js
+++ src/containers/Restaurants/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 import PropTypes from "prop-types";
 
-export const Restaurants = ({restaurant}) => {
-  const { name, logoUrl, city, foodTypes, 
-    phone, state, streetAddress, hours, zip } = restaurant;
-  const hoursInfo = Object.keys(hours).map((day, index) => {
-    return <p key={index}>{day}: {hours[day]}</p>;
-  });
-  return (
-    <div>
-      <h2>{name}</h2>
-      <img src={logoUrl} alt="restaurant logo"/>
-      <p>{streetAddress}</p>
-      <p>{`${city} ${state} ${zip}`}</p>
-      <p>{phone}</p>
-      <p>{`Food types: ${foodTypes}`}</p>
-      {hoursInfo}
-    </div>
-  );
-};
+export class Restaurants extends Component {
+
+  handleClick = () => {
+    console.log('works');
+  }
+
+  render() {
+    const { name, logoUrl, city, foodTypes, 
+      phone, state, streetAddress, hours, zip } = this.props.restaurant;
+    const hoursInfo = Object.keys(hours).map((day, index) => {
+      return <p key={index}>{day}: {hours[day]}</p>;
+    });
+    return (
+      <div onClick={this.handleClick}>
+        <h2>{name}</h2>
+        <img src={logoUrl} alt="restaurant logo"/>
+        <p>{streetAddress}</p>
+        <p>{`${city} ${state} ${zip}`}</p>
+        <p>{phone}</p>
+        <p>{`Food types: ${foodTypes}`}</p>
+        {hoursInfo}
+      </div>
+    );
+  }
+}
 
 Restaurants.propTypes = {
   restaurant: PropTypes.object
