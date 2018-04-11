e9c7abe101c1ece304cfa192bbb83d32744cdd05
Aldo Mercado <amercado1014@gmail>
2018-04-11 10:29:33 -0600

    Add display menu on click of restuarant

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './styles.css';
-import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export const Menu = () => {
+export const Menu = ({menu}) => {
+  const displayMenuSection = menu.map(section => {
+    const menuItem = section.items.map(item => {
+      return (
+        <div key={item.apiKey}>
+          <p>{item.name}</p>
+          {item.description &&
+          <p>{item.description}</p>
+          }
+          <p>{item.basePrice}</p>
+        </div>
+      );
+    });
+    return (
+      <div key={section.apiKey}>
+        <h3>{section.name}</h3>
+        {menuItem}
+      </div>
+    );
+  });
+
   return (
     <div>
-      Menu
+      {displayMenuSection}
     </div>
   );
 };
 
-export default Menu;
+export const mapStateToProps = state => ({
+  menu: state.menu
+});
+
+Menu.propTypes = {
+  menu: PropTypes.array
+};
+
+export default connect(mapStateToProps)(Menu);
