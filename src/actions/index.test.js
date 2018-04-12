09ceac9e44c3059f8682fe9d3c3b1f12f027f094
Aldo Mercado <amercado1014@gmail>
2018-04-12 07:12:15 -0600

    Add actions test and update snapshots

---
+++ src/actions/index.test.js
@@ -0,0 +1,31 @@
+import * as actions from './index';
+
+describe('actions', () => {
+
+  it('should create an action to add restaurants', () => {
+    const restaurants = [{restaurant: 1}, {restaurant: 2}];
+    const expectedAction = {
+      type: 'ADD_RESTAURANTS',
+      restaurants
+    };
+    expect(actions.addRestaurants(restaurants)).toEqual(expectedAction);
+  });
+
+  it('should create an action to add a menu', () => {
+    const menu = {items:[1, 2, 3]};
+    const expectedAction = {
+      type: 'ADD_MENU',
+      menu
+    };
+    expect(actions.addMenu(menu)).toEqual(expectedAction);
+  });
+
+  it('should create an action to add a restaurant', () => {
+    const restaurant = {name: 'Dominos'};
+    const expectedAction = {
+      type: 'ADD_RESTAURANT',
+      restaurant
+    };
+    expect(actions.addRestaurant(restaurant)).toEqual(expectedAction);
+  });
+});
