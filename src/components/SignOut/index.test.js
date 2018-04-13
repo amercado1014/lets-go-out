06ca587fc3feb89cd9f5c77b7b8cb023e75ac7fc
Aldo Mercado <amercado1014@gmail>
2018-04-13 06:42:30 -0600

    Add sign out tests

---
+++ src/components/SignOut/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignOut from '../SignOut';
+import { shallow } from 'enzyme';
+
+describe('SignOut', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<SignOut />);
+  });
+
+  it('should math the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
