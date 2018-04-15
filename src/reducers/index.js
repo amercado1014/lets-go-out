9db7552a9b9ef23310776ad2f6ea5376fc086a6c
Aldo Mercado <amercado1014@gmail>
2018-04-14 19:32:14 -0600

    Add favorites to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import menu from './menuReducer';
 import restaurant from './restaurantReducer';
 import locationStatus from './locationReducer';
 import authUser from './userReducer';
+import favorites from './favoritesReducer';
 
 const rootReducer = combineReducers({
   restaurants,
   menu,
   restaurant,
   locationStatus,
-  authUser
+  authUser,
+  favorites
 });
 
 export default rootReducer;
