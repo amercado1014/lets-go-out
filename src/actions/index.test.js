dece5a6871a7484f39a3b8c282bc28cacc169df0
Aldo Mercado <amercado1014@gmail>
2018-04-16 12:58:20 -0600

    Add all actions to tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -28,4 +28,47 @@ describe('actions', () => {
     };
     expect(actions.addRestaurant(restaurant)).toEqual(expectedAction);
   });
+
+  it('should create an action to set location off', () => {
+    const boolean = true;
+    const expectedAction = {
+      type: 'LOCATION_OFF',
+      boolean
+    };
+    expect(actions.locationOff(boolean)).toEqual(expectedAction);
+  });
+
+  it('should create an action to add user', () => {
+    const user = { name: 'user'};
+    const expectedAction = {
+      type: 'ADD_USER',
+      user
+    };
+    expect(actions.addUser(user)).toEqual(expectedAction);
+  });
+
+  it('should create an action to add favorite', () => {
+    const favorite = { apiKey: '1234'};
+    const expectedAction = {
+      type: 'ADD_FAVORITE',
+      favorite
+    };
+    expect(actions.addFavorite(favorite)).toEqual(expectedAction);
+  });
+
+  it('should create an action to remove favorite', () => {
+    const apiKey = '5678klm';
+    const expectedAction = {
+      type: 'REMOVE_FAVORITE',
+      apiKey
+    };
+    expect(actions.removeFavorite(apiKey)).toEqual(expectedAction);
+  });
+
+  it('should create an action to reset favorites', () => {
+    const expectedAction = {
+      type: 'RESET_FAVORITES'
+    };
+    expect(actions.resetFavorites()).toEqual(expectedAction);
+  });
 });
